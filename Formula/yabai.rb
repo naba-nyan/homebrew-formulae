9aab94bcd688cda03d3f711d15aac9cbbc76e419
INABA Shota <naba.key.0827@gmail.com>
2024-03-15 21:35:41 +0900

    add yabai v6.0.15

---
+++ Formula/yabai.rb
@@ -0,0 +1,47 @@
+class Yabai < Formula
+  desc "A tiling window manager for macOS based on binary space partitioning."
+  homepage "https://github.com/koekeishiya/yabai"
+  url "https://github.com/koekeishiya/yabai/releases/download/v6.0.15/yabai-v6.0.15.tar.gz"
+  sha256 "f3e8c077017b6206a7befd4db1bb4c2015afd2b87d2661eec0b5a6c221660eee"
+  head "https://github.com/koekeishiya/yabai.git"
+
+  depends_on :macos => :big_sur
+
+  def install
+    man.mkpath
+
+    if build.head?
+      system "make", "-j1", "install"
+    end
+
+    bin.install "#{buildpath}/bin/yabai"
+    (pkgshare/"examples").install "#{buildpath}/examples/yabairc"
+    (pkgshare/"examples").install "#{buildpath}/examples/skhdrc"
+    man1.install "#{buildpath}/doc/yabai.1"
+  end
+
+  def caveats; <<~EOS
+    Copy the example configuration into your home directory:
+      cp #{opt_pkgshare}/examples/yabairc ~/.yabairc
+      cp #{opt_pkgshare}/examples/skhdrc ~/.skhdrc
+
+    If you want yabai to be managed by launchd (start automatically upon login):
+      yabai --start-service
+
+    When running as a launchd service logs will be found in:
+      /tmp/yabai_<user>.[out|err].log
+
+    If you are using the scripting-addition; remember to update your sudoers file:
+      sudo visudo -f /private/etc/sudoers.d/yabai
+
+    Build the configuration row by running:
+      echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa"
+
+    README: https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(latest-release)#configure-scripting-addition
+    EOS
+  end
+
+  test do
+    assert_match "yabai-v#{version}", shell_output("#{bin}/yabai --version")
+  end
+end
--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## List of Packages
 
-| package name  | ver   | url                                                                 |
-| ------------- | ----- | ------------------------------------------------------------------- |
-| git-pompt.zsh | 2.3.0 | [woefe/git-prompt.zsh](https://github.com/woefe/git-prompt.zsh)     |
-| htop-vim      | 3.0.3 | [KoffeinFlummi/htop-vim](https://github.com/KoffeinFlummi/htop-vim) |
+| package name  | ver    | url                                                                 |
+| ------------- | -----  | ------------------------------------------------------------------- |
+| git-pompt.zsh | 2.3.0  | [woefe/git-prompt.zsh](https://github.com/woefe/git-prompt.zsh)     |
+| htop-vim      | 3.0.3  | [KoffeinFlummi/htop-vim](https://github.com/KoffeinFlummi/htop-vim) |
+| yabai         | 6.0.15 | [koekeishiya/yabai](https://github.com/koekeishiya/yabai)           |
